b1d017e79874f9b8fbcc700fe382ee06e2339d84
niram <nirambrt3@gmail.com>
2025-06-09 16:58:31 +0545

    fix body overflow

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,16 +1,16 @@
 import styles from './Nav.module.css';
 
 
-export default function Nav(){
-    return (
-        <>
-        <nav className={styles.navBar}>
-            <div className={styles.searchBox}>
-                <button className={styles.searchBtn}><i className="fas fa-magnifying-glass"></i></button>
-                <input type="text" className={styles.inputBox} placeholder="Search Food Item..."/>
-            </div>
-        </nav>
-        <div className={styles.spacer}></div>
-        </>
-    )
+export default function Nav() {
+         return (
+            <>
+            <nav className={styles.navBar}>
+                <div className={styles.searchBox}>
+                    <button className={styles.searchBtn}><i className="fas fa-magnifying-glass"></i></button>
+                    <input type="text" className={styles.inputBox} placeholder="Search Food Item..."/>
+                </div>
+            </nav>
+            <div className={styles.spacer}></div>
+            </>
+        ) 
 }
