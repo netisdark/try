5f5701d33b138bd63746983e740e98dc30ddec2d
niram <nirambrt3@gmail.com>
2025-06-10 06:13:20 +0545

    Fix footer position

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Banner from './components/Banner';
+import Banner from './components/menu/Banner';
+import Footer from './components/menu/Footer';
 
-import Home from './components/Home';
-import Nav from './components/Nav';
-
+import Home from './components/menu/Menu';
+import Nav from './components/menu/Nav';
+import MenuNav from './components/menu/MenuNav'
 export default function App(){
   return (
     <BrowserRouter>
      <Nav/>
      <Banner/>
+     <MenuNav/>
         <Routes>
             <Route path='/' element = {<Home/>}/>
         </Routes>
+        <Footer/>
     </BrowserRouter>
   )
 }
