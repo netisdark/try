6c8a188731ec7273572f1606f9b2e0104d6b8354
niram <nirambrt3@gmail.com>
2025-06-15 07:42:21 +0545

    Build menu page

--- cafeAdvert/src/components/menu/Menu.jsx
+++ cafeAdvert/src/components/menu/Menu.jsx
@@ -1,9 +1,35 @@
 import styles from './Menu.module.css';
+import MenuItems from './menuItems'; 
 
+export default function Menu() {
+  return (
+    <div className={styles.menuCont}>
+      <span className={styles.menuTitle}>What We Serve</span>
+      <p className={styles.menuDesc}>
+        From starters to sweet endings, this is your go-to guide for everything we bring to the table.
+      </p>
 
+      {MenuItems.map(({ category, items }) => (
+          <div className={styles.menuGrid}>
+            {items.map((item, index) => {
+              const itemImgClass = styles[`itemImg${index + 1}`];
 
-export default function Menu(){
-    return (
-        <div>This is menu page.</div>
-    )
+              return (
+                <div
+                  key={item.name}
+                  className={`${itemImgClass} ${styles.itemImg}`}
+                  style={{
+                    backgroundImage: `url(${item.img})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                  }}
+                >
+                  <div className={styles.itemName}>{item.name}</div>
+                </div>
+              );
+            })}
+          </div>
+      ))}
+    </div>
+  );
 }
---
+++ cafeAdvert/src/components/menu/menuItems.js
@@ -0,0 +1,15 @@
+const menuItems = [
+    {
+        category: 'coffee',
+        items: [
+            { name: 'Brewed Delights', img: 'https://imgs.search.brave.com/TITRllLfivLJzF0xK-BonlduCVmGuQoXOo50BFc4L0c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzZjLzZh/LzRmLzZjNmE0ZmUy/OWNkNGZiNTc2M2Vi/YTRmMWJhN2M3MjNh/LmpwZw' },
+            {name : 'Starters', img : 'https://imgs.search.brave.com/8hxYYZoRiecXqKPcsnOHpk8OAnACCqU-Tp7PDdGf9DQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvNjM3/MTYxMjU4L3Bob3Rv/L2RlbGljYXRlLW1l/ZGFsbGlvbnMtb2Yt/dmVhbC13aXRoLXZl/Z2V0YWJsZXMuanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPUJL/ZnBRWEpIa1hSZDkx/MzMyZndvS0ZSZTZt/bXZsOWQ2NEhlaDVZ/SXdCMTA9'},
+            {name : 'Sea Foods', img : 'https://imgs.search.brave.com/isyyDYOaUcI-Qx94m6nLXo1rabtW0D5lbYlEHZIn_rg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAzLzMwLzk0Lzc5/LzM2MF9GXzMzMDk0/Nzk2Nl80N2FsSGhn/aFlidmprdU00YzBM/NXNVWE9Wa0xlU3dp/TS5qcGc'},
+            {name : 'Main Course', img : 'https://imgs.search.brave.com/Sr9fOCPa_46Z7csq7pYQFEvyAlbsNJ1iUkk_6zkv3V0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTIx/ODA5MjExNS9waG90/by9hc3NvcnRlZC1k/aXNoZXMtdG9wLXZp/ZXctcG9yay1yaWJz/LWJlZWYtc3RlYWst/c2FsbW9uLXN0ZWFr/LWNoaWNrZW4td2lu/Z3MtYnVyZ2VyLXdp/dGgtbWVhdC5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9MTVL/TEpRdGNqR1ZlbE93/amhFLU1tNklMUHpn/YXZLZHA2dmpDQXBt/RURscz0'},
+            {name : 'Sweet Desserts', img : 'https://imgs.search.brave.com/xMQulO1Y9aLq5-6L_FNp6CIoKNN12hOxeKlECY93eJE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly90aGVj/dXJseXNwb29uYmxv/Zy5jb20vd3AtY29u/dGVudC91cGxvYWRz/LzIwMjMvMDMvOC13/aGl0ZS1jaG9jb2xh/dGUtcm9zZS1tYWNh/cm9ucy1hZXN0aGV0/aWMtcGluay1kZXNz/ZXJ0cy03MjB4MTA4/MC5qcGc'}       
+            
+        ]
+    }
+];
+
+export default menuItems;
