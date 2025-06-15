3610bb5f393cbba7b4710ada4eed21640155d2d6
niram <nirambrt3@gmail.com>
2025-06-15 10:08:32 +0545

    add foot routes

--- cafeAdvert/src/components/footer/Footer.jsx
+++ cafeAdvert/src/components/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import styles from './Footer.module.css';
-
+import {NavLink} from 'react-router'
 
 
 export default function Footer() {
@@ -10,11 +10,11 @@ export default function Footer() {
                 <div className={styles.nameDesc}>&copy;All rights reserved</div>
             </div>
             <div className={styles.footItemsCont}>
-                <div className={styles.itemTitle}>Quick Links</div>
+                <div className={styles.itemTitle}>Quick NavLinknks</div>
                 <ul className={styles.footItems}>
-                    <li className={styles.footItem}>Menu</li>
-                    <li className={styles.footItem}>About</li>
-                    <li className={styles.footItem}>Contact</li>
+                    <NavLink to='/menu' className={styles.footItem}>Menu</NavLink>
+                    <NavLink to='/about' className={styles.footItem}>About</NavLink>
+                    <NavLink to='/contact' className={styles.footItem}>Contact</NavLink>
                 </ul>
             </div>
             <div className={styles.footItemsCont}>
@@ -22,6 +22,7 @@ export default function Footer() {
                 <ul className={styles.footItems}>
                     <li className={styles.footItem}>+977 9812345678</li>
                     <li className={styles.footItem}>cafe@gmail.com</li>
+                    <li className={styles.footItem}>Kathmandu - Bouddha</li>
                 </ul>
             </div>
         </footer>
