80f4dc7a5e884dab5c5c8922e1343e6d7ea09b9e
niram <nirambrt3@gmail.com>
2025-06-15 10:04:15 +0545

    build contact page

--- cafeAdvert/src/components/contact/Contact.jsx
+++ cafeAdvert/src/components/contact/Contact.jsx
@@ -4,6 +4,12 @@ import styles from './Contact.module.css';
 
 export default function Contact(){
     return(
-        <div>This is contact page.</div>
+        <div className={styles.contactBox}>
+        <div className={styles.queryTitle}>Send us your query</div>
+                 <input type="text" placeholder = "Enter your name" required/>
+                 <input type="email" placeholder="Enter your email" required/>
+                 <textarea placeholder="Let us know how can we help..." required></textarea>
+                 <button className={styles.sendBtn}>Send Message</button>
+         </div>
     )
 }
