f64158186211436546e74784e858c9fe2db41e63
niram <nirambrt3@gmail.com>
2025-06-15 08:30:26 +0545

    about design-1

--- cafeAdvert/src/components/about/About.jsx
+++ cafeAdvert/src/components/about/About.jsx
@@ -4,6 +4,14 @@ import styles from './About.module.css';
 
 export default function About(){
     return(
-        <div>This is about page.</div>
+        <div className={styles.bannerCont}>
+            <div className={styles.banner}></div>
+            <div className={styles.bannerDescCont}>
+                <span className={styles.bannerTitle}>Why Cafe Italio Was Built</span>
+                <p className={styles.bannerDesc}>
+                Cafe Italio was established with the vision of offering a refined culinary experience in an inviting and intimate setting. Designed to blend the sophistication of fine dining with the warmth of a cozy atmosphere, the café provides guests with a thoughtfully curated menu, exceptional service, and a space where quality and comfort come together seamlessly.
+                </p>
+            </div>
+        </div>
     )
 }
