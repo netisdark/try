a701b45f7df77407c6b976199125309c4f4452a1
niram <nirambrt3@gmail.com>
2025-06-16 15:13:31 +0545

    build tables page

--- frontDesk/src/components/nav/Nav.jsx
+++ frontDesk/src/components/nav/Nav.jsx
@@ -1,20 +1,62 @@
 import styles from './Nav.module.css';
-import { NavLink } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
-
-export default function Nav(){
-    return( 
-    <nav className={styles.navCont}>
-
+export default function Nav() {
+    return (
+        <nav className={styles.navCont}>
             <div className={styles.cafeName}>Cafeto Italio</div>
 
-        <div className={styles.navLinksCont}>
-            <NavLink to='/' className={styles.navLink}><i class="fa-solid fa-couch"></i><span className={styles.navItem}>Tables</span></NavLink>
-            <NavLink to='/orders' className={styles.navLink}><i class="fa-solid fa-list-ul"></i><span className={styles.navItem}>Orders</span></NavLink>
-            <NavLink to='/bill' className={styles.navLink}><i class="fa-solid fa-money-check-dollar"></i><span className={styles.navItem}>Bill</span></NavLink>
-            <NavLink to='/menu' className={styles.navLink}><i class="fa-solid fa-file-lines"></i><span className={styles.navItem}>Menu</span></NavLink>
-            <NavLink to='/payment' className={styles.navLink}><i class="fa-solid fa-file-lines"></i><span className={styles.navItem}>Payments</span></NavLink>
-        </div>
-    </nav>
-    )
+            <div className={styles.navLinksCont}>
+                <NavLink
+                    to='/'
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+                    }
+                >
+                    <i className="fa-solid fa-couch"></i>
+                    <span className={styles.navItem}>Tables</span>
+                </NavLink>
+
+                <NavLink
+                    to='/orders'
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+                    }
+                >
+                    <i className="fa-solid fa-list-ul"></i>
+                    <span className={styles.navItem}>Orders</span>
+                </NavLink>
+
+                <NavLink
+                    to='/bill'
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+                    }
+                >
+                    <i className="fa-solid fa-money-check-dollar"></i>
+                    <span className={styles.navItem}>Bill</span>
+                </NavLink>
+
+                <NavLink
+                    to='/menu'
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+                    }
+                >
+                    <i className="fa-solid fa-file-lines"></i>
+                    <span className={styles.navItem}>Menu</span>
+                </NavLink>
+
+                <NavLink
+                    to='/payment'
+                    className={({ isActive }) =>
+                        isActive ? `${styles.navLink} ${styles.active}` : styles.navLink
+                    }
+                >
+                    <i className="fa-solid fa-file-lines"></i>
+                    <span className={styles.navItem}>Payments</span>
+                </NavLink>
+            </div>
+        </nav>
+    );
 }
--- frontDesk/src/components/tables/Tables.jsx
+++ frontDesk/src/components/tables/Tables.jsx
@@ -1,36 +1,49 @@
 import styles from './Tables.module.css';
 
-
 export default function Tables() {
-    return <div className={styles.tableWrapper}>
-        <div className={styles.pageTitle}>Tables</div>
-        <div className={styles.tableCont}>
-
-            <div className={styles.tableCol1}>
-                <span className={styles.tbCol1}><i class="fa-solid fa-couch"></i></span>
-                <span className={`${styles.tbCol1} ${styles.active}`}><i class="fa-solid fa-couch"></i></span>
-                <span className={styles.tbCol1}><i class="fa-solid fa-couch"></i></span>
-                <span className={`${styles.tbCol1} ${styles.active}`}><i class="fa-solid fa-couch"></i></span>
+    return (
+        <div className={styles.tableWrapper}>
+            <div className={styles.titleCont}>
+                <div className={styles.title}>Tables</div>
             </div>
 
-            <div className={styles.cashierCont}>
-                <span className={styles.cashierTable}><i class="fa-solid fa-couch"></i></span>
-                <i class="fa-solid fa-computer"></i>
-            </div>
+            <div className={styles.tableMap}>
 
-            <div className={styles.tableCol2}>
-                <span className={`${styles.tbCol2} ${styles.active}`}><i class="fa-solid fa-couch"></i></span>
-                <span className={styles.tbCol2}><i class="fa-solid fa-couch"></i></span>
-                <span className={styles.tbCol2}><i class="fa-solid fa-couch"></i></span>
-            </div>
+                <div className={styles.col1}>
+                    <div className={`${styles.col1tb1} ${styles.tbCol1}`}>
+                        <button className={styles.statusBall}>1</button>
+                    </div>
+                    <div className={`${styles.col1tb2} ${styles.tbCol1}`}></div>
+                    <div className={`${styles.col1tb3} ${styles.tbCol1}`}></div>
+                    <div className={`${styles.col1tb4} ${styles.tbCol1}`}>
+                    <button className={styles.statusBall}>4</button>
+                    </div>
+                    <div className={`${styles.col1tb5} ${styles.tbCol1}`}></div>
+                </div>
 
-            <div className={styles.tableCol3}>
-                <span className={styles.tbCol3}><i class="fa-solid fa-chair"></i></span>
-                <span className={`${styles.tbCol3} ${styles.active}`}><i class="fa-solid fa-chair"></i></span>
-                <span className={styles.tbCol3}><i class="fa-solid fa-chair"></i></span>
-                <span className={styles.tbCol3}><i class="fa-solid fa-chair"></i></span>
-                <span className={`${styles.tbCol3} ${styles.active}`}><i class="fa-solid fa-chair"></i></span>
+                <div className={styles.col2}>
+                    <div className={`${styles.col2tb1} ${styles.tbCol2}`}></div>
+                    <div className={styles.cashCounter}></div>
+                    <div className={`${styles.col2tb2} ${styles.tbCol2}`}></div>
+                </div>
+
+                <div className={styles.col3}>
+                    <div className={`${styles.col3tb2} ${styles.tbCol3}`}>
+                    <button className={styles.statusBall}>8</button>
+                    </div>
+                    <div className={`${styles.col3tb3} ${styles.tbCol3}`}></div>
+                    <div className={`${styles.col3tb4} ${styles.tbCol3}`}></div>
+                </div>
+
+                <div className={styles.col4}>
+                    <div className={`${styles.col4tb1} ${styles.tbCol4}`}>
+                    <button className={styles.statusBall}>11</button>
+                    </div>
+                    <div className={`${styles.col4tb2} ${styles.tbCol4}`}></div>
+                    <div className={`${styles.col4tb3} ${styles.tbCol4}`}></div>
+                </div>
+
             </div>
         </div>
-    </div>
+    );
 }
