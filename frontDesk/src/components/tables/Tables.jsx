316d64743fe68339249c21ef4537a620a4169f2e
niram <nirambrt3@gmail.com>
2025-06-17 06:21:44 +0545

    dynamic table generation

--- frontDesk/src/components/tables/Tables.jsx
+++ frontDesk/src/components/tables/Tables.jsx
@@ -1,49 +1,41 @@
 import styles from './Tables.module.css';
 
+const tablesConfig = {
+  col1: 5,
+  col2: 3,
+  col3: 3,
+  col4: 3,
+};
+
 export default function Tables() {
-    return (
-        <div className={styles.tableWrapper}>
-            <div className={styles.titleCont}>
-                <div className={styles.title}>Tables</div>
-            </div>
+  let tableNumber = 1;
 
-            <div className={styles.tableMap}>
+  return (
+    <div className={styles.tableWrapper}>
+      <div className={styles.titleCont}>
+        <div className={styles.title}>Tables</div>
+      </div>
 
-                <div className={styles.col1}>
-                    <div className={`${styles.col1tb1} ${styles.tbCol1}`}>
-                        <button className={styles.statusBall}>1</button>
-                    </div>
-                    <div className={`${styles.col1tb2} ${styles.tbCol1}`}></div>
-                    <div className={`${styles.col1tb3} ${styles.tbCol1}`}></div>
-                    <div className={`${styles.col1tb4} ${styles.tbCol1}`}>
-                    <button className={styles.statusBall}>4</button>
-                    </div>
-                    <div className={`${styles.col1tb5} ${styles.tbCol1}`}></div>
+      <div className={styles.tableMap}>
+        {Object.entries(tablesConfig).map(([colKey, tableCount], colIndex) => (
+          <div key={colKey} className={styles[colKey]}>
+            {[...Array(tableCount)].map((_, tableIndex) => {
+              const tableClass = `${styles[`${colKey}tb${tableIndex + 1}`]} ${styles[`tbCol${colIndex + 1}`]}`;
+              const currentTableNumber = tableNumber++;
+
+              return (
+                <div key={currentTableNumber} className={tableClass}>
+                    {/* 
+                        toggle this display property (with {display : 'none'}) to denote active and in-active tables... 
+                        make the table active when order is placed, in-active when payment is confirmed...
+                    */}
+                  <button style={{display : 'block'}} className={styles.statusBall}>{currentTableNumber}</button>
                 </div>
-
-                <div className={styles.col2}>
-                    <div className={`${styles.col2tb1} ${styles.tbCol2}`}></div>
-                    <div className={styles.cashCounter}></div>
-                    <div className={`${styles.col2tb2} ${styles.tbCol2}`}></div>
-                </div>
-
-                <div className={styles.col3}>
-                    <div className={`${styles.col3tb2} ${styles.tbCol3}`}>
-                    <button className={styles.statusBall}>8</button>
-                    </div>
-                    <div className={`${styles.col3tb3} ${styles.tbCol3}`}></div>
-                    <div className={`${styles.col3tb4} ${styles.tbCol3}`}></div>
-                </div>
-
-                <div className={styles.col4}>
-                    <div className={`${styles.col4tb1} ${styles.tbCol4}`}>
-                    <button className={styles.statusBall}>11</button>
-                    </div>
-                    <div className={`${styles.col4tb2} ${styles.tbCol4}`}></div>
-                    <div className={`${styles.col4tb3} ${styles.tbCol4}`}></div>
-                </div>
-
-            </div>
-        </div>
-    );
+              );
+            })}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
