c10dd993e12055c872b81e60cd5b0c93410ba360
niram <nirambrt3@gmail.com>
2025-06-20 13:26:49 +0545

    Build Nav

--- client/admin/src/App.jsx
+++ client/admin/src/App.jsx
@@ -1,6 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Nav from './components/nav/Nav';
+import Dashboard from './components/dashboard/Dashboard';
+import Account from './components/account/Account';
+import Payments from './components/payments/Payments';
 
-
-export default function App(){
-  return <div>App</div>
+export default function App() {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      backgroundColor : '#eeeeee'
+      }}>
+    <BrowserRouter>
+    <Nav/>
+      <Routes>
+        <Route path="/" element={<Dashboard/>} />
+        <Route path="/payments" element={<Payments/>} />
+        <Route path="/account" element={<Account/>} />
+      </Routes>
+    </BrowserRouter>
+    </div>
+  )
 }
